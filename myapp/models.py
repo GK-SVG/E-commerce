a84cebbcc1ae2e5bda2ebbaedff315bdc1417b37
gk-svg <gk32239@gmail.com>
2020-06-13 23:17:10 +0530

    get_total_item function added

--- myapp/models.py
+++ myapp/models.py
@@ -44,7 +44,12 @@ class Order(models.Model):
         orderitems=self.orderitem_set.all()
         total=sum([item.get_total for item in orderitems])
         return total
-    
+
+    @property
+    def get_cart_items(self):
+        orderitems=self.orderitem_set.all()
+        total=sum([item.get_total for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product=models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
