acded020fabe3802cf89ba4166f8850b3b0ee074
gk-svg <gk32239@gmail.com>
2020-06-16 18:34:27 +0530

    Models changes due to User

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Customer(models.Model):
-    user=models.OneToOneField(User,null=True,blank=True,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
     name=models.CharField(max_length=120,null=True)
     email=models.CharField(max_length=120)
 
@@ -31,13 +31,22 @@ class Product(models.Model):
         
 
 class Order(models.Model):
-    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
+    customer=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
     date_ordered=models.DateTimeField(auto_now_add=True)
     complete=models.BooleanField(max_length=100,null=True)
     transaction_id=models.CharField(max_length=100,null=True)
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def shipping(self):
+        shipping=False
+        orderitems=self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital==False:
+                shipping=True
+        return shipping
     
     @property
     def grand_total(self):
@@ -67,7 +76,7 @@ class OrderItem(models.Model):
     
 
 class ShippingAddress(models.Model):
-    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
+    customer=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
     order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
     address=models.CharField(max_length=200,null=False)
     city=models.CharField(max_length=150,null=False)
