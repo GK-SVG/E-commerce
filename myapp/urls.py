14008c389c6084f85455722f86cf7293e4fb64dc
gk-svg <gk32239@gmail.com>
2020-06-16 18:36:56 +0530

    user forms url added

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('checkout',views.checkout,name='checkout') ,  
     path('update_item',views.updateItem,name='UpdateItem'),
     path('process_order',views.processOrder,name='ProcessOrder'),
+    path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
 ]
