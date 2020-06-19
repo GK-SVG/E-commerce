e38927aba34434f7a8dd19f86d7f1b5a5378c79f
gk-svg <gk32239@gmail.com>
2020-06-19 22:50:21 +0530

    heroku deployement

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,3 +126,12 @@ STATICFILES_DIRS = (
 # managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
+
+import django_heroku
+django_heroku.settings(locals())
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
